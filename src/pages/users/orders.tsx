cccc22cfebb31cc7650dff34a732ccfd6f56ea8b
alimprasetyo77 <palim695@gmail.com>
2024-01-25 18:51:54 +0700

    fix : error

--- src/pages/users/orders.tsx
+++ src/pages/users/orders.tsx
@@ -24,9 +24,9 @@ const Orders = () => {
       setLoading(false);
     }
   };
-  const handleCancelOrder = async (id: string) => {
+  const handleCancelOrder = async (id: number) => {
     try {
-      const result = await cancelOrder(id);
+      const result = await cancelOrder(`${id}`);
       getDataOrders();
       alert(result.message);
     } catch (error) {
@@ -99,7 +99,7 @@ const Orders = () => {
                               <Alert
                                 title="Are you sure?"
                                 description={`This action cannot be undone. This will permanently delete the order.`}
-                                onAction={() => handleCancelOrder(`${data.order_id}`)}>
+                                onAction={() => handleCancelOrder(data.order_id)}>
                                 <button className="py-1 px-4 bg-rose-500 text-white font-medium text-xs rounded">
                                   Cancel Order
                                 </button>
