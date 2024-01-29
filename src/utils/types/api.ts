0a9438d5cdb852122552c84f8f1f4f35d97b86ff
gilangr1440 <gilangr1440@gmail.com>
2024-01-29 20:55:07 +0700

    commit

--- src/utils/types/api.ts
+++ src/utils/types/api.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { IProductsUser } from "../apis/products/types"
+import { IProductsUser } from "../apis/products/types";
 
 export interface Response {
-  code: number
-  message: string
+  code: number;
+  message: string;
 }
 export interface ResponsePayload<T = any> {
-  message: string,
-  data: T
+  message: string;
+  data: T;
 }
 export interface ResponseProductsUser {
-  Product: IProductsUser[]
+  Product: IProductsUser[];
 }
